36056afd69613ffabb32363fe94dc3a55e2080e3
Lee Barney <barney.cit@gmail.com>
2014-09-12 14:09:48 -0600

    first checkin

---
+++ first_hello/first_hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  first_hello
+//
+//  Created by lee on 9/12/14.
+//  Copyright (c) 2014 Lee Barney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("We are doing stuff with Swift!");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
